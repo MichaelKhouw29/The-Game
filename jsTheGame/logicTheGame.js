cf3c70047e09ff179b56d23a645d85b2bb22b551
Michael <michael_ruben@hotmail.com>
2018-09-23 11:39:07 +0200

    Added index.html and jsTheGame/logicTheGame.js

---
+++ jsTheGame/logicTheGame.js
@@ -0,0 +1,32 @@
+
+
+    if(prompt('Welk type is effectief tegen grass type? Rock type of ice type?') === 'ice type'){
+   	   if(prompt('Welk type heeft geen effect op flying type? Poison type of ground type?') === 'ground type'){
+   		  if(prompt('Welk type heeft weinig effect op dark type? Phsycic type of bug type?') === 'phsycic type'){
+   			 if(prompt('Welk type is de volgende move: Scald. Fire type of water type?') === 'water type'){
+   			    if(prompt('Welke move heeft een poison status effect? Sludge bomb of venoschock?') === 'sludge bomb'){
+   			   	   if(prompt('Welke move kan meerdere tegenstanders raken? Razor leaf of steel wing?') === 'razor leaf'){
+   			   		  if(prompt('Is wild charge een electric move of een normal move?') === 'electric move'){
+   			   		  	 if(prompt('Welke move kan elke aanval tegenhouden? Reflect of protect?') === 'protect'){
+                            if(prompt('is trick room een dark move of een phsycic move?') === 'phsycic move'){
+                               if(prompt('Welk type is effectief tegen ghost type? Ghost type of fighting type?') === 'ghost type'){
+
+                               	       }else('Rank:Meester. Perfect, je bent een Pokemon meester');
+
+                                    }else('Rank:Legend. Ongeloofelijk, je bent er bijna.');
+
+   			   		  	        }else alert('Rank:Ace. WOW, blijf doorgaan.');
+
+   			   		        }else alert('Rank:Expert. Indrukwekkend, Heel goed van je.');
+   			   		 
+   			   	        }else alert('Rank:Pro. Geweldig, zie je dat je het kan?');
+
+                    }else alert('Rank:Onderzoeker. Mooi, je bent op de helft.');
+
+   			    }else alert('Rank:Novice. Niet slecht, maar je moet nog veel leren.');   
+
+   		    }else alert('Rank:Groentje. Niet zo goed, maar het is niet slecht voor een groentje.');
+
+   	    }else alert('Rank:Beginner. Oei, volgende keer beter.');
+
+    }else alert('Rank:Nieuwkomer. Jammer, probeer het opnieuw.');
