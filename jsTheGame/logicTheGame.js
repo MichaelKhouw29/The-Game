f474a8625ea3a01744ca17e64ab945a4bd649465
Michael <michael_ruben@hotmail.com>
2018-11-18 16:50:52 +0100

    een 'failure' teller toegepast

--- jsTheGame/logicTheGame.js
+++ jsTheGame/logicTheGame.js
@@ -1,32 +1,49 @@
 
+var failure = 0;
 
-    if(prompt('Welk type is effectief tegen grass type? Rock type of ice type?') === 'ice type'){
-   	   if(prompt('Welk type heeft geen effect op flying type? Poison type of ground type?') === 'ground type'){
-   		  if(prompt('Welk type heeft weinig effect op dark type? Phsycic type of bug type?') === 'phsycic type'){
-   			 if(prompt('Welk type is de volgende move: Scald. Fire type of water type?') === 'water type'){
-   			    if(prompt('Welke move heeft een poison status effect? Sludge bomb of venoschock?') === 'sludge bomb'){
-   			   	   if(prompt('Welke move kan meerdere tegenstanders raken? Razor leaf of steel wing?') === 'razor leaf'){
-   			   		  if(prompt('Is wild charge een electric move of een normal move?') === 'electric move'){
-   			   		  	 if(prompt('Welke move kan elke aanval tegenhouden? Reflect of protect?') === 'protect'){
-                            if(prompt('is trick room een dark move of een phsycic move?') === 'phsycic move'){
-                               if(prompt('Welk type is effectief tegen ghost type? Ghost type of fighting type?') === 'ghost type'){
+    if(prompt('Welk type is effectief tegen grass type?') != 'rock type' || 'ice type'){
+      addfailure();
+    if(prompt('Welk type heeft geen effect op flying type?') != 'fighting type' || 'ground type'){
+      addfailure();
+    if(prompt('Welk type heeft weinig effect op dark type?') != 'phsycic type' || 'bug type'){
+      addfailure();
+    if(prompt('Welk type is de volgende move: Scald.') != 'fire type' || 'water type'){
+      addfailure();
+    if(prompt('Welke move heeft een poison status effect?') != 'sludge bomb' || 'venoschock'){
+      addfailure();
+    if(prompt('Welke move kan meerdere tegenstanders raken?') != 'razor leaf' || 'night slash'){
+      addfailure();
+    if(prompt('Is wild charge een electric move of een normal move?') != 'electric move' || 'normal move'){
+      addfailure();
+    if(prompt('Welke move kan elke aanval tegenhouden?') != 'reflect' || 'protect'){
+      addfailure();
+    if(prompt('is trick room een dark move of een phsycic move?') != 'phsycic move' || 'dark move'){
+      addfailure();
+    if(prompt('Welk type is effectief tegen ghost type?') != 'dragon type' || 'ghost type'){
+      addfailure();
+                      }
 
-                               	       }else('Rank:Meester. Perfect, je bent een Pokemon meester');
+                    }
 
-                                    }else('Rank:Legend. Ongeloofelijk, je bent er bijna.');
+   			   	      }
 
-   			   		  	        }else alert('Rank:Ace. WOW, blijf doorgaan.');
-
-   			   		        }else alert('Rank:Expert. Indrukwekkend, Heel goed van je.');
+   			        }
    			   		 
-   			   	        }else alert('Rank:Pro. Geweldig, zie je dat je het kan?');
+   			      }
 
-                    }else alert('Rank:Onderzoeker. Mooi, je bent op de helft.');
+            }
 
-   			    }else alert('Rank:Novice. Niet slecht, maar je moet nog veel leren.');   
+   	      } 
 
-   		    }else alert('Rank:Groentje. Niet zo goed, maar het is niet slecht voor een groentje.');
+        }
 
-   	    }else alert('Rank:Beginner. Oei, volgende keer beter.');
+   	  }
 
-    }else alert('Rank:Nieuwkomer. Jammer, probeer het opnieuw.');
+    }
+
+function addfailure(){
+  console.log(failure++);
+  if (failure == 3) {
+    alert("Game Over. Ververs de pagina om het opnieuw te proberen.");
+  }
+}
